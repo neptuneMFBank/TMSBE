d9c46cd31dafd6049ad2f31ae3b9e63c1f96c02a
Samo <mac@localhost>
2024-03-07 18:56:39 +0100

    Added sql file

---
+++ fineract-provider/src/main/resources/db/changelog/tenant/parts/0148_m_appuser_merchant_mapping.sql
@@ -0,0 +1,30 @@
+--
+-- Licensed to the Apache Software Foundation (ASF) under one
+-- or more contributor license agreements. See the NOTICE file
+-- distributed with this work for additional information
+-- regarding copyright ownership. The ASF licenses this file
+-- to you under the Apache License, Version 2.0 (the
+-- "License"); you may not use this file except in compliance
+-- with the License. You may obtain a copy of the License at
+--
+-- http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing,
+-- software distributed under the License is distributed on an
+-- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+-- KIND, either express or implied. See the License for the
+-- specific language governing permissions and limitations
+-- under the License.
+--
+
+CREATE TABLE `m_appuser_merchant_mapping` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `appuser_id` bigint NOT NULL,
+  `client_id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `appuser_id_client_id` (`appuser_id`,`client_id`),
+  UNIQUE KEY `unique_self_client` (`client_id`),
+  CONSTRAINT `m_merchant_appuser_id` FOREIGN KEY (`appuser_id`) REFERENCES `m_appuser` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `m_merchant_client_id` FOREIGN KEY (`client_id`) REFERENCES `m_client` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+

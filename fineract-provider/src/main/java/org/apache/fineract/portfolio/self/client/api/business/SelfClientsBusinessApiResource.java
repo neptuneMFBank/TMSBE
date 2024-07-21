dee913aff784231f9f7bd5c1f582c8676c2b319c
olakunlethompson <olakunlethompson@localhost>
2024-07-21 19:25:29 +0100

    Updates Code on Get Client All Forms of Transactions

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/self/client/api/business/SelfClientsBusinessApiResource.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/self/client/api/business/SelfClientsBusinessApiResource.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -330,7 +330,7 @@ public class SelfClientsBusinessApiResource {
     @Path("transactions")
     @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
-    public String retrieveVisibileSavingsProduct(@PathParam("clientId") final Long clientId, @Context final UriInfo uriInfo,
+    public String retrieveClientTransactions(@PathParam("clientId") final Long clientId, @Context final UriInfo uriInfo,
                                                  @QueryParam("transactionTypeId") @Parameter(description = "transactionTypeId e.g 12- Loans, 23- Wallets/Savings") final Long transactionTypeId,
                                                  @QueryParam("depositAccountTypeId") @Parameter(description = "depositAccountTypeId e.g 100- Savings Account, 200- Fixed Account, 300- Recurring Account") Integer depositAccountTypeId,
                                                  @QueryParam("offset") @Parameter(description = "offset") final Integer offset,

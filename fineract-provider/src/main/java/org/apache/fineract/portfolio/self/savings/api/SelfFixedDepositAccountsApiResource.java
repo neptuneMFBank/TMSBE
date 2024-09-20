7ce7457ef642d49815c55f022f7e22daf16ea443
Chibuikem29 <chibuikem@simplifytechnologies.io>
2024-09-20 09:25:25 +0100

    add endpoints to get single recurring and fixed deposit transaction self

---
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/self/savings/api/SelfFixedDepositAccountsApiResource.java
@@ -0,0 +1,93 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.fineract.portfolio.self.savings.api;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.fineract.infrastructure.security.service.PlatformSecurityContext;
+import org.apache.fineract.portfolio.savings.api.FixedDepositAccountTransactionsApiResource;
+import org.apache.fineract.portfolio.savings.exception.SavingsAccountNotFoundException;
+import org.apache.fineract.portfolio.self.client.service.AppuserClientMapperReadService;
+import org.apache.fineract.portfolio.self.savings.data.SelfFixedDepositDataValidator;
+import org.apache.fineract.portfolio.self.savings.service.AppuserSavingsMapperReadService;
+import org.apache.fineract.useradministration.domain.AppUser;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+
+@Path("/self/fixeddepositaccounts")
+@Component
+@Scope("singleton")
+@Tag(name = "Self Fixed Deposit Account", description = "")
+public class SelfFixedDepositAccountsApiResource {
+
+    private final PlatformSecurityContext context;
+    private final SelfFixedDepositDataValidator dataValidator;
+    private final AppuserClientMapperReadService appUserClientMapperReadService;
+    private final FixedDepositAccountTransactionsApiResource fixedDepositAccountTransactionsApiResource;
+    private final AppuserSavingsMapperReadService appuserSavingsMapperReadService;
+
+    public SelfFixedDepositAccountsApiResource(PlatformSecurityContext context, SelfFixedDepositDataValidator dataValidator, AppuserClientMapperReadService appUserClientMapperReadService, FixedDepositAccountTransactionsApiResource fixedDepositAccountTransactionsApiResource, AppuserSavingsMapperReadService appuserSavingsMapperReadService) {
+        this.context = context;
+        this.dataValidator = dataValidator;
+        this.appUserClientMapperReadService = appUserClientMapperReadService;
+        this.fixedDepositAccountTransactionsApiResource = fixedDepositAccountTransactionsApiResource;
+        this.appuserSavingsMapperReadService = appuserSavingsMapperReadService;
+    }
+
+    @GET
+    @Path("{accountId}/transactions/{transactionId}")
+    @Consumes({ MediaType.APPLICATION_JSON })
+    @Produces({ MediaType.APPLICATION_JSON })
+    @Operation(summary = "Retrieve Fixed Deposit Account Transaction", description = "Retrieves Fixed Deposit Account Transaction\n\n"
+            + "Example Requests:\n" + "\n" + "self/fixeddepositaccounts/1/transactions/1")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = SelfSavingsApiResourceSwagger.GetSelfSavingsAccountsAccountIdTransactionsTransactionIdResponse.class))) })
+    public String retrieveFixedDepositTransaction(@PathParam("accountId") @Parameter(description = "accountId") final Long accountId,
+                                                  @PathParam("transactionId") @Parameter(description = "transactionId") final Long transactionId,
+                                                  @Context final UriInfo uriInfo) {
+
+        this.dataValidator.validateRetrieveFixedDepositTransaction(uriInfo);
+
+        validateAppuserSavingsAccountMapping(accountId);
+
+        return this.fixedDepositAccountTransactionsApiResource.retrieveOne(accountId, transactionId, uriInfo);
+    }
+
+    private void validateAppuserSavingsAccountMapping(final Long accountId) {
+        AppUser user = this.context.authenticatedUser();
+        final boolean isMappedSavings = this.appuserSavingsMapperReadService.isSavingsMappedToUser(accountId, user.getId());
+        if (!isMappedSavings) {
+            throw new SavingsAccountNotFoundException(accountId);
+        }
+    }
+}
---
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/self/savings/api/SelfRecurringDepositAccountsApiResource.java
@@ -0,0 +1,92 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.fineract.portfolio.self.savings.api;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.fineract.infrastructure.security.service.PlatformSecurityContext;
+import org.apache.fineract.portfolio.savings.api.RecurringDepositAccountTransactionsApiResource;
+import org.apache.fineract.portfolio.savings.exception.SavingsAccountNotFoundException;
+import org.apache.fineract.portfolio.self.client.service.AppuserClientMapperReadService;
+import org.apache.fineract.portfolio.self.savings.data.SelfRecurringDepositDataValidator;
+import org.apache.fineract.portfolio.self.savings.service.AppuserSavingsMapperReadService;
+import org.apache.fineract.useradministration.domain.AppUser;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+
+@Path("/self/recurringdepositaccounts")
+@Component
+@Scope("singleton")
+@Tag(name = "Self Recurring Deposit Account", description = "")
+public class SelfRecurringDepositAccountsApiResource {
+
+    private final PlatformSecurityContext context;
+    private final SelfRecurringDepositDataValidator dataValidator;
+    private final AppuserClientMapperReadService appUserClientMapperReadService;
+    private final RecurringDepositAccountTransactionsApiResource recurringDepositAccountTransactionsApiResource;
+    private final AppuserSavingsMapperReadService appuserSavingsMapperReadService;
+
+    public SelfRecurringDepositAccountsApiResource(PlatformSecurityContext context, SelfRecurringDepositDataValidator dataValidator, AppuserClientMapperReadService appUserClientMapperReadService, RecurringDepositAccountTransactionsApiResource recurringDepositAccountTransactionsApiResource, AppuserSavingsMapperReadService appuserSavingsMapperReadService) {
+        this.context = context;
+        this.dataValidator = dataValidator;
+        this.appUserClientMapperReadService = appUserClientMapperReadService;
+        this.recurringDepositAccountTransactionsApiResource = recurringDepositAccountTransactionsApiResource;
+        this.appuserSavingsMapperReadService = appuserSavingsMapperReadService;
+    }
+
+    @GET
+    @Path("{accountId}/transactions/{transactionId}")
+    @Consumes({ MediaType.APPLICATION_JSON })
+    @Produces({ MediaType.APPLICATION_JSON })
+    @Operation(summary = "Retrieve Recurring Deposit Account Transaction", description = "Retrieves Fixed Deposit Account Transaction\n\n"
+            + "Example Requests:\n" + "\n" + "self/recurringdepositaccounts/1/transactions/1")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = SelfSavingsApiResourceSwagger.GetSelfSavingsAccountsAccountIdTransactionsTransactionIdResponse.class))) })
+    public String retrieveRecurringDepositTransaction(@PathParam("accountId") @Parameter(description = "accountId") final Long accountId,
+                                                      @PathParam("transactionId") @Parameter(description = "transactionId") final Long transactionId,
+                                                      @Context final UriInfo uriInfo) {
+
+        this.dataValidator.validateRetrieveRecurringDepositTransaction(uriInfo);
+
+        validateAppuserSavingsAccountMapping(accountId);
+
+        return this.recurringDepositAccountTransactionsApiResource.retrieveOne(accountId, transactionId, uriInfo);
+    }
+
+    private void validateAppuserSavingsAccountMapping(final Long accountId) {
+        AppUser user = this.context.authenticatedUser();
+        final boolean isMappedSavings = this.appuserSavingsMapperReadService.isSavingsMappedToUser(accountId, user.getId());
+        if (!isMappedSavings) {
+            throw new SavingsAccountNotFoundException(accountId);
+        }
+    }
+}
